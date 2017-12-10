74dd079dc5cb1c24fed0c69b7e6c732e7e6e7cc8
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-09 19:24:43 -0500

    Added enableRawMode Function

---
+++ kilo.c
@@ -0,0 +1,21 @@
+#include <uunistd.h>
+#include <termios.h>
+
+void enableRawMode();
+
+int main() {
+	enableRawMode();
+	
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1);
+	return 0;
+}
+
+void enableRawMode(){
+	struct terminos raw;
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
